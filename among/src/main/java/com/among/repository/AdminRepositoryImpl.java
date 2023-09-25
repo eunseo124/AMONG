8a6c992b1183caedf29c54aa2a79a9bb7fc9161a
Administrator <Administrator@User-2023VPVUFZ>
2023-09-25 16:49:03 +0900

    커밋

---
+++ among/src/main/java/com/among/repository/AdminRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.among.repository;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.among.domain.Member;
+
+public class AdminRepositoryImpl implements AdminRepository {
+	
+	private JdbcTemplate template;
+
+	@Override
+	public List<Member> getAllMemberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> getmflist(String keyField) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setupmem(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
